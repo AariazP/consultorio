b0099ae505e398021782f4ac88fd217a1422157e
Alejandro Arias <josea.ariasp@uqvirtual.edu.co>
2024-04-02 18:50:20 -0500

    (UPDATE) dockerfile was created

---
+++ src/main/java/org/arias/consultorio/Controller.java
@@ -0,0 +1,18 @@
+package org.arias.consultorio;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/hola")
+public class Controller {
+
+
+    @GetMapping("/mundo")
+    public String holaMundo() {
+        return "Hola Mundo 2";
+    }
+}
